68a1dce877e4126ef686edbc8a4e6e679ea8146d
hbakergo <hbakergoveia@hotmail.com>
2022-07-05 17:58:04 -0300

    Services

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent},
+  { path: 'list', component: ListRenderComponent}
+];
+
+@NgModule({
+  declarations:[],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
